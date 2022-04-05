7c94a839eb8084904d0550e9edb04a59cd1140f1
oleg-tgn <stelmah.taganrog@gmail.com>
2022-04-05 21:50:46 +0300

    Add navbar and fix bootsrap adaptive styles

--- header.php
+++ header.php
@@ -1,33 +1,88 @@
 <div class="navbar-top-wrap">
-      <div class="container cont1">        
-        <p class="top_desc">ООО &laquo;Альфа-Сертификат&raquo; ОФОРМЛЕНИЕ ВСЕХ ВИДОВ РАЗРЕШИТЕЛЬНЫХ ДОКУМЕНТОВ НА <span>ЛЮБЫЕ</span> ИЗДЕЛИЯ, СЫРЬЕ И ОБОРУДОВАНИЕ</p>
-        <!-- <div class="top_mail">
-            <a class="mailLink" href="mailto:info@sk-sertifikat.ru">info@sk-sertifikat.ru</a><br>
-            <a class="mailLink" href="tel:+79043433544">+7 (904) 343-35-44</a> -->
-        <!-- </div> --> 
+  <div class="container cont1">        
+    <p class="top_desc">ООО &laquo;Альфа-Сертификат&raquo; ОФОРМЛЕНИЕ ВСЕХ ВИДОВ РАЗРЕШИТЕЛЬНЫХ ДОКУМЕНТОВ НА <span>ЛЮБЫЕ</span> ИЗДЕЛИЯ, СЫРЬЕ И ОБОРУДОВАНИЕ</p>
+    <!-- <div class="top_mail">
+        <a class="mailLink" href="mailto:info@sk-sertifikat.ru">info@sk-sertifikat.ru</a><br>
+        <a class="mailLink" href="tel:+79043433544">+7 (904) 343-35-44</a> -->
+    <!-- </div> --> 
 
-        <div class="top_address">   
-          Ростовская обл., г. Таганрог<br> ул. Москатова, д. 15
-          <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
-        </div>
-
-        <div class="top_phone">   
-          <a class="mailLink" href="mailto:info_alfasert@mail.ru">info_alfasert@mail.ru</a><br>
-          <a class="mailLink" href="tel:+79043433544">+7 (904) 343-35-44</a>      
-          <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
-        </div>
-      </div>
+    <div class="top_address">   
+      Ростовская обл., г. Таганрог<br> ул. Москатова, д. 15
+      <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
     </div>
-      <div class="serviceMenu">
-        <div class="container">
-          <div class="serviceMenu__list">
-            <a href="/" class="serviceMenu__logo-link"><img class="serviceMenu__logo" alt="Альфа Сертификат" src="/img/logob.svg"></a>
-            <a href="/#sertification-types" class="smLink teamHref">Сертификация</a>
-            <a href="/#algorithm" class="smLink teamHref">Алгоритм</a>
-            <a href="/#guarantee" class="smLink teamHref">Преимущества</a>
-            <a href="/#thanks" class="smLink teamHref">Благодарности</a>            
-            <a href="/#faq" class="smLink teamHref">Вопросы</a>                    
-            <a href="/#contacts" class="smLink teamHref">Контакты</a>
-          </div>
+
+    <div class="top_phone">   
+      <a class="mailLink" href="mailto:info_alfasert@mail.ru">info_alfasert@mail.ru</a><br>
+      <a class="mailLink" href="tel:+79043433544">+7 (904) 343-35-44</a>      
+      <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
+    </div>
+  </div>
+</div>
+
+<!-- <div class="serviceMenu">
+  <div class="container">
+    <div class="serviceMenu__list">
+      <a href="/" class="serviceMenu__logo-link"><img class="serviceMenu__logo" alt="Альфа Сертификат" src="/img/logob.svg"></a>
+      <a href="/#sertification-types" class="smLink teamHref">Сертификация</a>
+      <a href="/#algorithm" class="smLink teamHref">Алгоритм</a>
+      <a href="/#guarantee" class="smLink teamHref">Преимущества</a>
+      <a href="/#thanks" class="smLink teamHref">Благодарности</a>            
+      <a href="/#faq" class="smLink teamHref">Вопросы</a>                    
+      <a href="/#contacts" class="smLink teamHref">Контакты</a>
+    </div>
+  </div>
+</div> -->
+
+<nav class="navbar navbar-inverse serviceMenu">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand serviceMenu__logo-link" href="#"><img class="bottom-menu__link" alt="Альфа Сертификат" src="/img/logow.svg"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
         </div>
-      </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
