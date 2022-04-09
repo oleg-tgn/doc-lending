c82db52ae89fc7ec0640546937c49a72f389d32a
oleg-tgn <stelmah.taganrog@gmail.com>
2022-04-09 14:10:40 +0300

    Update navbar. Add active menu on scroll

--- header.php
+++ header.php
@@ -37,53 +37,28 @@
 <nav class="navbar navbar-inverse serviceMenu">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand serviceMenu__logo-link" href="#"><img class="bottom-menu__link" alt="Альфа Сертификат" src="/img/logow.svg"></a>
-    </div>
+    <div class="serviceMenu__container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand serviceMenu__logo-link" href="/"><img class="bottom-menu__link" alt="Альфа Сертификат" src="/img/logow.svg"></a>
+      </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav serviceMenu__links">
+          <li data-link="sertification-types"><a href="/#sertification-types" class="serviceMenu__link">Сертификация</a></li>
+          <li data-link="algorithm"><a href="/#algorithm" class="serviceMenu__link">Алгоритм</a></li>
+          <li data-link="guarantee"><a href="/#guarantee" class="serviceMenu__link">Преимущества</a></li>
+          <li data-link="thanks"><a href="/#thanks" class="serviceMenu__link">Благодарности</a></li>
+          <li data-link="faq"><a href="/#faq" class="serviceMenu__link">Вопросы</a></li>
+          <li data-link="contacts"><a href="/#contacts" class="serviceMenu__link">Контакты</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div>
   </div><!-- /.container-fluid -->
 </nav>
