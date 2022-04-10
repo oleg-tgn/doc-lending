d31129578e9dd61b3d895d87ec656a6a1b6b23e6
oleg-tgn <stelmah.taganrog@gmail.com>
2022-04-10 21:30:10 +0300

    Fix styles

--- footer.php
+++ footer.php
@@ -1,129 +1,94 @@
-
-    <footer class="bg">
-<section class="footer-top mt-4">
-        <div class="feedback_block" >
-            <div class="container px-0 px-xl-3">
-                <div class="col-12">
-                    <div class="row">
-                        <div class="col-12 col-lg-6">
-                            <div class="footer-notice d-flex flex-column align-items-center align-items-xl-start w-100">
-                                <div class="img position-relative pb-0 pt-3 pb-xl-3">
-                                        <img src="/img/sert.png"
-                                             alt="Сертификация вашей продукции"
-                                             title="Сертификация вашей продукции"
-                                             class="img-fluid"/>
-                                </div>
-
-                            </div>
-                        </div>
-
-                        <div class="col-12 col-lg-5">
-          <div class="feedback-c mt-lg-n5  bg footer_popup" id="feedback">
-            <div>
-              <h3 class="footer_textpopup">Обратная связь</h3>
-              <?php
-                if (isset($_GET['mail']) && $_GET['mail'] == 'success'):
-              ?>
-              <div class="alert alert-success" role="alert">
-                  Спасибо за ваш отзыв!
+<footer class="bg">
+  <section class="footer-top mt-4">
+    <div class="feedback_block">
+      <div class="container px-0 px-xl-3">
+        <div class="row">
+          <div class="col-12 col-lg-6">
+            <div class="footer-notice d-flex flex-column align-items-center align-items-xl-start w-100">
+              <div class="img position-relative pb-0 pt-3 pb-xl-3">
+                <img src="/img/sert.png" alt="Сертификация вашей продукции" title="Сертификация вашей продукции" class="img-fluid" />
               </div>
-              <?php else: ?>
-                <p class="footer_textpopup">Напишите свой отзыв о нашей работе:</p>
-                <form action="/mail.php" method="POST" class="modal_form footer_form">
+
+            </div>
+          </div>
+          <div class="col-12 col-lg-1"></div>
+          <div class="col-12 col-lg-5">
+            <div class="feedback-c mt-lg-n5  bg footer_popup" id="feedback">
+              <div>
+                <h3 class="footer_textpopup">Обратная связь</h3>
+                <?php
+                if (isset($_GET['mail']) && $_GET['mail'] == 'success') :
+                ?>
+                  <div class="alert alert-success" role="alert">
+                    Спасибо за ваш отзыв!
+                  </div>
+                <?php else : ?>
+                  <p class="footer_textpopup">Напишите свой отзыв о нашей работе:</p>
+                  <form action="/mail.php" method="POST" class="modal_form footer_form">
                     <input class="modal_input footer_inputpopup" type="text" name="feedback_name" required placeholder="Ваше имя">
                     <input class="modal_input footer_inputpopup" type="email" name="feedback_email" required placeholder="Ваш Email">
                     <textarea class="modal_input footer_inputpopup" name="feedback_text" id="" cols="30" rows="5" placeholder="Ваш отзыв"></textarea>
                     <button type="submit" class="btn btn_top btn_feedback" onclick="wsKiller.checkFormOnSubmit($(this).closest('form'))">Отправить</button>
-                </form>
-              <?php endif; ?>
+                  </form>
+                <?php endif; ?>
+              </div>
             </div>
           </div>
-
-                           </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </section>
-
-
-    <script>
-      var elements = document.querySelectorAll('.phoneClassFooter');
-      elements.forEach(function(el, index) {
-      window.intlTelInput(el, {
-        autoHideDialCode: false,
-        dropdownContainer: document.body,
-        initialCountry: "auto",
-        geoIpLookup: function(callback) {
-          $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-            var countryCode = (resp && resp.country) ? resp.country : "";
-            callback(countryCode);
-          });
-        },
-        nationalMode: false,
-        onlyCountries: ['ru', 'by', 'ua', 'kz'],
-        placeholderNumberType: "MOBILE",
-        preferredCountries: ['ru'],
-        utilsScript: "https://www.sk-sertifikat.ru/js/utils.js",
-      });
-      });
-    </script> 
-
-
-
-
-
-    <section class="footer-bottom">
-        <div class="bottom-menu-wrapper">
-          <div class="container">
-            <ul class="bottom-menu list-unstyled py-2">
-              <li class="bottom-menu__item">
-                <a href="/" class="bottom-menu__link"><img class="bottom-menu__link" alt="Альфа Сертификат" src="/img/logow.svg"></a>
-              </li>
-
-              <li class="bottom-menu__item">
-                <a href="/#sertification-types" class="bottom-menu__link">Сертификация</a>
-              </li>
-              <li class="bottom-menu__item">
-                <a href="/#algorithm" class="bottom-menu__link">Алгоритм</a>
-              </li>
-              <li class="bottom-menu__item">
-                <a href="/#guarantee" class="bottom-menu__link">Преимущества</a>
-              </li>
-              <li class="bottom-menu__item">
-                <a href="/#thanks" class="bottom-menu__link">Благодарности</a>
-              </li>
-              <li class="bottom-menu__item">
-                <a href="/#faq" class="bottom-menu__link">Вопросы</a>
-              </li>
-              <li class="bottom-menu__item">
-                <a href="/#contacts" class="bottom-menu__link">Контакты</a>
-              </li>
-            </ul>
-          </div>
       </div>
+    </div>
+  </section>
 
+  <section class="footer-bottom">
+    <div class="bottom-menu-wrapper">
       <div class="container">
-        <p class="top_desc top_desc--bottom">ООО &laquo;Альфа-Сертификат&raquo; оформление всех видов разрешительных документов на <span>любые</span> изделия, сырье и оборудование</p>
-        <!-- <div class="top_mail">
+        <ul class="bottom-menu list-unstyled py-2">
+          <li class="bottom-menu__item">
+            <a href="/" class="bottom-menu__link"><img class="bottom-menu__link" alt="Альфа Сертификат" src="/img/logow.svg"></a>
+          </li>
+
+          <li class="bottom-menu__item">
+            <a href="/#sertification-types" class="bottom-menu__link">Сертификация</a>
+          </li>
+          <li class="bottom-menu__item">
+            <a href="/#algorithm" class="bottom-menu__link">Алгоритм</a>
+          </li>
+          <li class="bottom-menu__item">
+            <a href="/#guarantee" class="bottom-menu__link">Преимущества</a>
+          </li>
+          <li class="bottom-menu__item">
+            <a href="/#thanks" class="bottom-menu__link">Благодарности</a>
+          </li>
+          <li class="bottom-menu__item">
+            <a href="/#faq" class="bottom-menu__link">Вопросы</a>
+          </li>
+          <li class="bottom-menu__item">
+            <a href="/#contacts" class="bottom-menu__link">Контакты</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="container">
+      <p class="top_desc top_desc--bottom">ООО &laquo;Альфа-Сертификат&raquo; оформление всех видов разрешительных документов на <span>любые</span> изделия, сырье и оборудование</p>
+      <!-- <div class="top_mail">
             <a class="mailLink" href="mailto:info@sk-sertifikat.ru">info@sk-sertifikat.ru</a><br>
             <a class="mailLink" href="tel:+79043433544">+7 (904) 343-35-44</a> -->
-        <!-- </div> --> 
+      <!-- </div> -->
 
-        <div class="top_address top_address--bottom">   
+      <div class="top_address top_address--bottom">
         <?= $ADDRESS ?>
-          <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
-        </div>
+        <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
+      </div>
 
-        <div class="top_phone top_phone--bottom">   
-          <a class="mailLink_top mailLink--bottom" href="mailto:info@sk-sertifikat.ru">info_alfasert@mail.ru</a><br>
-          <a class="mailLink mailLink--bottom" href="tel:+79043433544">+7 (904) 343-35-44</a>      
-          <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
-        </div>
-      </div>   
-            </div>
-        </div>
+      <div class="top_phone top_phone--bottom">
+        <a class="mailLink_top mailLink--bottom" href="mailto:info@sk-sertifikat.ru">info_alfasert@mail.ru</a><br>
+        <a class="mailLink mailLink--bottom" href="tel:+79043433544">+7 (904) 343-35-44</a>
+        <!-- <p class="phone_perezvon" data-toggle="modal" data-target="#modalPerezvon">Перезвоните мне →</p> -->
+      </div>
     </div>
-</section>
+    </div>
+    </div>
+    </div>
+  </section>
 </footer>
-
--- head.php
+++ head.php
@@ -30,9 +30,9 @@
 
 <link rel="canonical" href="index.htm">
 
-<link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/logow180.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/logow32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/logow16.png">
 <link rel="manifest" href="/img/favicons/site.webmanifest">
 <link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
 
