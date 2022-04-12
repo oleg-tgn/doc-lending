e366868e0b41d976d0819afb529aa3416f0ec8c3
dSAndr <iiredii@mail.ru>
2022-04-12 22:29:50 +0300

    fix feedback, add tel input

--- mail.php
+++ mail.php
@@ -1,67 +1,50 @@
 <?php
-require_once "SendMailSmtpClass.php"; // подключаем класс
-$mailSMTP = new SendMailSmtpClass('sert.alfa@yandex.ru', '78Tis4#', 'ssl://smtp.yandex.ru', 465, "UTF-8");
-// $mailSMTP = new SendMailSmtpClass('логин', 'пароль', 'хост', 'порт', 'кодировка письма');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-// от кого
-$from = array(
-    "Alfa Sert", // Имя отправителя
-    "info_alfasert@mail.ru" // почта отправителя
-);
-// кому
-$to = 'stelmah.oleg@ya.ru';
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
-$name  = $_POST['feedback_name'];
-$email = $_POST['feedback_email'];
-$text  = $_POST['feedback_text'];
-
-$message = "<h1>Обратная связь</h1>
-<p><b>Имя:</b> $name </p>
-<p><b>Email:</b> $email</p>
-<p><b>Сообщение:</b> $text</p>";
-
-// отправляем письмо
-$result =  $mailSMTP->send($to, 'Обратная связь', $message, $from);
-// $result =  $mailSMTP->send('Кому письмо', 'Тема письма', 'Текст письма', 'Отправитель письма');
+$mail = new PHPMailer(true);
 
 $url = parse_url($_SERVER['HTTP_REFERER']);
-$new_url = 'https://'.$url['host'].$url['path'];
+$new_url = 'http://'.$url['host'].$url['path'];
 
-if ($result === true) {
-    $new_url .= '?mail=success#feedback';
-    header("Location: ".$new_url);
-} else {
-    $new_url.= '?mail=error&message='.$result.'#feedback';
-    header("Location: ".$new_url);
-}
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.yandex.ru';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'sert.alfa@yandex.ru';                     //SMTP username
+    $mail->Password   = '78Tis4#';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-exit;
+    //Recipients
+    $mail->setFrom('sert.alfa@yandex.ru', 'Alfa Sert');  // от кого
+    $mail->addAddress('iiredii@mail.ru', 'Name'); // кому
+   
+    $name  = $_POST['feedback_name'];
+    $email = $_POST['feedback_email'];
+    $text  = $_POST['feedback_text'];
+    $message = "<h1>Обратная связь</h1>
+                <p><b>Имя:</b> $name </p>
+                <p><b>Email:</b> $email</p>
+                <p><b>Сообщение:</b> $text</p>";
 
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Обратная связь';
+    $mail->Body    = $message;
 
-
-// пример использования
-require_once "SendMailSmtpClass.php"; // подключаем класс
-
-$mailSMTP = new SendMailSmtpClass('sert.alfa', '78Tis4#', 'ssl://smtp.yandex.ru', 'stelmah.oleg', 465);
-// $mailSMTP = new SendMailSmtpClass('логин', 'пароль', 'хост', 'имя отправителя');
-
-$name  = $_POST['name'];
-$email = $_POST['email'];
-$text  = $_POST['text'];
-
-$message = "<h1>Обратная связь</h1>
-<p><b>Имя:</b> $name </p>
-<p><b>Email:</b> $email</p>
-<p><b>Сообщение:</b> $text</p>";
-
-// заголовок письма
-$headers= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
-$headers .= "From: Alfa Sertificat <stelmah.oleg@yandex.ru>\r\n"; // от кого письмо
-$result =  $mailSMTP->send('stelmah.oleg@yandex.ru', 'Обратная связь', $message, $headers); // отправляем письмо
-// $result =  $mailSMTP->send('Кому письмо', 'Тема письма', 'Текст письма', 'Заголовки письма');
-if($result === true){
-    echo "Письмо успешно отправлено";
-}else{
-    echo "Письмо не отправлено. Ошибка: " . $result;
+    if ($mail->send()) {
+        $new_url .= '?mail=success#feedback';
+        header("Location: ".$new_url);
+    }    
+} catch (Exception $e) {
+    $new_url.= '?mail=error&message='.$mail->ErrorInfo.'#feedback';
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
