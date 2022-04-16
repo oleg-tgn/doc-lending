957bd1e2c5fd60bdfc2b9fa4448269c2a1dde780
oleg-tgn <stelmah.taganrog@gmail.com>
2022-04-16 11:14:39 +0300

    Add fancybox sertificat

--- include/guarantees.php
+++ include/guarantees.php
@@ -1,3 +1,7 @@
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css"
+/>
 <section class="row-fluid row3" id="guarantee">
   <div class="container-fluid">
     <div class="guarantee__row">
@@ -7,7 +11,9 @@
       <div class="col-12 col-md-6">
         <h2 class="guarantee__title">Почему вам стоит работать именно с&nbsp;нами?</h2>
         <ol class="guarantee__list">
-          <li class="care_p1">Мы аттестованный орган по <a data-fancybox href="#hidden">Сертификации</a>;</li>
+          <li class="care_p1">Мы аттестованный орган
+            (<a href="/img/Acreditation.jpg" data-fancybox="gallery" data-caption="Аттестат аккредитиации органа по сертификации">Аттестат аккредитиации</a>,
+             <a href="/img/aplication_acreditation.jpg" data-fancybox="gallery" data-caption="Перечень услуг, характеристик и документов, на соответвии которым проводитсядобровольная сертификация">приложение</a>)</li>
           <li class="care_p1">Команда высококласных специалистов с многолетним опытом;</li>
           <li class="care_p1">Доставка оригиналов документов в течении 5 рабочих дней;</li>
           <li class="care_p1">Не теряем связь с клиентами, в том числе после завершения проекта;</li>
@@ -15,38 +21,12 @@
         </ol>
         <button class="btn btn_top anim-btn js-open-callbackkiller">Связаться с нами</button>
         <div class="cl"></div>
-      </div> 
+      </div>
     </div>
   </div>
 </section>
- 
-<div class="acreditationbox" id="hidden">
-
-    <!-- Slider main container -->
-    <div class="swiper">
-      <!-- Additional required wrapper -->
-      <div class="swiper-wrapper">
-
-        <!-- Slides -->
-        <div class="swiper-slide">
-          <img  class="attestation" alt="Аттестат аккредитации" src="/img/Acreditation.jpg">
-        </div>
-        <div class="swiper-slide">
-          <img  class="attestation" alt="Аттестат аккредитации" src="/img/aplication acreditation.jpg">
-        </div>
-
-      </div>
-
-      <!-- If we need navigation buttons -->
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
-
-    </div>
 
 
 
 
-
-
-  
-</div>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
