a3b8ffe4711fbe71c5250e0de307a930705efe18
dSAndr <iiredii@mail.ru>
2022-05-04 20:25:36 +0300

    fix telephone

--- variables.php
+++ variables.php
@@ -2,5 +2,5 @@
 $ADDRESS = 'Ростовская обл., г. Таганрог<br> ул. Москатова, д. 15, оф. 12';
 $EMAIL = "info@alfa-sertificat.ru";
 $SITE_NAME = "Альфа Сертификат";
-$TELEPHONE = "8 800 201-19-35";
+$TELEPHONE = "8 800 201-92-35";
  ?>
