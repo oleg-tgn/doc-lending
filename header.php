c948a68ab0d5cdafc70172f0a55395b6a00331fb
dSAndr <iiredii@mail.ru>
2022-05-09 04:18:52 +0300

    done icon reght and fix adaptiv smallest mobile

--- header.php
+++ header.php
@@ -3,6 +3,14 @@
 
     <div class="descwrap">
       <p class="top_desc">ООО &laquo;Альфа-Сертификат&raquo; оформление всех видов разрешительных документов на <span>любые</span> изделия, сырье и оборудование</p>
+      <div class="top_telephone top_telephone__mobile">
+        <a class="telephone_link" href="tel:+78002011935">
+          <?= $TELEPHONE ?>
+        </a><br>
+        <a class="heder_callmy js-open-callbackkiller" href="#">
+          Перезвоните мне
+        </a>
+      </div>
       <div class="top_address top_address__laptop">
         <?= $ADDRESS ?>
       </div>
@@ -12,7 +20,7 @@
       <?= $ADDRESS ?>
     </div>
 
-    <div class="top_telephone">
+    <div class="top_telephone top_telephone__computer">
       <a class="telephone_link" href="tel:+78002011935">
         <?= $TELEPHONE ?>
       </a><br>
@@ -24,20 +32,20 @@
     <div class="top_phone">
       <div class="socweb">
         <a class="mailLink mailLink_top" href="mailto:<?=$EMAIL?>">
-          <img class="tel_logo" alt="mail" src="/img/icons/mail.svg">
           <?=$EMAIL?>
+          <img class="tel_logo" alt="mail" src="/img/icons/mail.svg">
         </a>
       </div>
       <div class="socweb">
         <a class="mailLink mailLink_top" target="_blank" href="https://telegram.me/Alfa_Sert_Trilion">
-          <img class="tel_logo" alt="Telegram" src="/img/icons/telegram-1.svg">
           Написать в Telegram
+          <img class="tel_logo" alt="Telegram" src="/img/icons/telegram-1.svg">
         </a>
       </div>
       <div class="socweb">
         <a class="mailLink mailLink_top" target="_blank" href="https://wa.me/+79043433544">
-          <img class="tel_logo" alt="WhatsApp" src="/img/icons/whatsapp-symbol.svg">
           Написать в WhatsApp
+          <img class="tel_logo" alt="WhatsApp" src="/img/icons/whatsapp-symbol.svg">
         </a>
       </div>
     </div>
