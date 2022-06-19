fa526f52491e0d7ce5f3654aa2d3e6cb6bb1496b
oleg-tgn <stelmah.taganrog@gmail.com>
2022-06-19 18:44:58 +0300

    Add IP adress

--- include/title-section.php
+++ include/title-section.php
@@ -14,18 +14,23 @@
   // }
 ?>
 
-<script>
-  window.onload = function () {
-      jQuery("#user-city").text(ymaps.geolocation.city);
-      jQuery("#user-region").text(ymaps.geolocation.region);
-      jQuery("#user-country").text(ymaps.geolocation.country);
-  }
-</script>
+<?php
+include("include/geo/SxGeo.php");
+$SxGeo = new SxGeo('include/geo/SxGeoCity.dat', SXGEO_BATCH | SXGEO_MEMORY); // Самый производительный режим, если нужно обработать много IP за раз
+
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$geoCity = $SxGeo->getCity($ip);
+$city = "Таганрог";
+if ($geoCity) {
+  $city =  $geoCity['city']['name_ru'];
+}
+?>
 
 <div class="container cont1_1">
   <div class="col-md-7 no-pad">
     <h1>Помощь в оформлении cертификатов любой сложности от 1 дня</h1>
-    <p class="first_desc">За 1 день в г. <span id="user-city">Таганрог</span> <span>сертифицируем все виды продукции</span> с&nbsp;внесением в&nbsp;реестр ФСА и гарантией легитимности</p>
+    <p class="first_desc">За 1 день в г. <?=$city?> <span>сертифицируем все виды продукции</span> с&nbsp;внесением в&nbsp;реестр ФСА и гарантией легитимности</p>
     <div class="col-xs-4 utp">
       <img alt="Альфа Сертификат" src="img/icons/utp_1.png">
       <p class="utp_text"><span>Отправим оригинал <br>бесплатно</span> в любую <br>точку РФ за 5 дней</p>
