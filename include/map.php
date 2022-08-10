d2f57a5b962bd79680dcf433ac382d593557abcd
oleg-tgn <stelmah.taganrog@gmail.com>
2022-08-11 00:09:34 +0300

    Update map address desktop

--- include/map.php
+++ include/map.php
@@ -1,5 +1,5 @@
 <div id="map" class="map map_desktop">
-  <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A715d25e3fd3a60f9a9dc3435a17793b0776b3ae36f0a1f34de3db292db2c83f8&amp;width=100%25&amp;height=518&amp;lang=ru_RU&amp;scroll=false"></script>
+  <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A5f96d002b7f1cb3c7f5112a9f54ca885559ef04024228c664c507c57ebbf6e77&amp;width=100%25&amp;height=518&amp;lang=ru_RU&amp;scroll=true"></script>
 </div>
 
 <div id="map" class="map map_mobile">
