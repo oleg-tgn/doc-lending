0e29f4852c1f24c1e752eeed81a58812282dbb55
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2022-11-22 23:08:35 +0400

    show email and change phone number

--- variables.php
+++ variables.php
@@ -2,5 +2,5 @@
 $ADDRESS = 'Ростовская обл., г. Ростов-на-Дону<br> улица Береговая, дом 16 Б';
 $EMAIL = "info@alfa-sertificat.ru";
 $SITE_NAME = "Альфа Сертификат";
-$TELEPHONE = "8 800 201-92-35";
+$TELEPHONE = "8 800 201 10 51";
  ?>
