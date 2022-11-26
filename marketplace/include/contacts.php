22e7da83b658a7a341e75a2458f3e49f0b7a62ab
Oleg Stelmakh <oleg.stelmakh@kinseed.com>
2022-11-26 16:36:46 +0400

    update phone and email marketplace

--- marketplace/include/contacts.php
+++ marketplace/include/contacts.php
@@ -8,11 +8,11 @@
     <div class="col-sm-4 footer_item">
       <img alt="Альфа Сертификат" src="img/icons/footer_2v2.png">
 
-      <div class="footer_item__hiddenPhone" id="hiddenPhoneFooterItem"> 8 800... <a href="#" onclick="event.preventDefault(); showHiddenInfo('#hiddenPhoneFooterItem', '#phoneFooterItem')">Показать</a> </div>
+      <!-- <div class="footer_item__hiddenPhone" id="hiddenPhoneFooterItem"> 8 800... <a href="#" onclick="event.preventDefault(); showHiddenInfo('#hiddenPhoneFooterItem', '#phoneFooterItem')">Показать</a> </div> -->
       <a id="phoneFooterItem" class="footer_item__call_link" href="tel:+74952041287">
         <?= $TELEPHONE ?>
       </a>
-      <div class="footer_item__hiddenPhone" id="hiddenEmailFooterItem"> info@... <a href="#" onclick="event.preventDefault(); showHiddenInfo('#hiddenEmailFooterItem', '#emailFooterItem')">Показать</a> </div>
+      <!-- <div class="footer_item__hiddenPhone" id="hiddenEmailFooterItem"> info@... <a href="#" onclick="event.preventDefault(); showHiddenInfo('#hiddenEmailFooterItem', '#emailFooterItem')">Показать</a> </div> -->
       <a id="emailFooterItem" class="footer_item__call_link" href="mailto:info@alfa-sertificat.ru">info@alfa-sertificat.ru</a>
     </div>
     <div class="col-sm-4 footer_item">
